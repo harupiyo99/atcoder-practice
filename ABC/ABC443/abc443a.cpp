4acd73758da0b48978a172f55d4cccb0d0821a6a
harupiyo-mech <harukigcm@gmail.com>
2026-02-01 20:00:38 +0900

    Create abc443_a.cpp

---
+++ ABC/ABC443/abc443a.cpp
@@ -0,0 +1,28 @@
+#include <iostream>   
+#include <vector>    
+#include <string>    
+#include <algorithm>
+#include <utility> 
+#include <map>       
+#include<queue>
+#include<cmath>
+#include<iomanip>
+
+using namespace std;
+using ll = long long int;
+using vi = vector<int>;
+using vvi = vector<vector<int>>; 
+using msi = map<string, int>;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string S;
+    cin >> S;
+
+    S.push_back('s');
+    cout << S << "\n";
+    return 0;
+}
