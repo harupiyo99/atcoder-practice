fa5ec5d9eb1620ac5b0be7d06e258c0f74467a6d
harupiyo99 <harukigcm@gmail.com>
2026-02-18 23:28:44 +0900

    Create: abc441d.cpp

---
+++ ABC/ABC441/abc441d.cpp
@@ -0,0 +1,25 @@
+#include <iostream>   
+#include <vector>    
+#include <string>    
+#include <algorithm>
+#include <utility> 
+#include <map>       
+#include<queue>
+#include<cmath>
+#include<iomanip>
+
+using namespace std;
+using ll = long long int;
+using vi = vector<int>;
+using vs = vector<string>;
+using vvi = vector<vector<int>>; 
+using pii = pair<int,int>;
+using psi = pair<string,int>;
+using msi = map<string, int>;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}
