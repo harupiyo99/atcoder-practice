bf5692c07f4f707f8740849f35287f95ccbba37d
harupiyo99 <harukigcm@gmail.com>
2026-02-25 15:08:27 +0900

    refactor: Updated abc445c.cpp

--- ABC/ABC445/abc445c.cpp
+++ ABC/ABC445/abc445c.cpp
@@ -1,4 +1,5 @@
 // https://atcoder.jp/contests/abc445/tasks/abc445_c
+// 最初は前からやっていきましたが、後ろからやっていったほうが効率的だったため、後ろから処理していく解法に変更しました。
 
 #include <iostream>   
 #include <vector>    
@@ -11,49 +12,44 @@
 #include<iomanip>
 
 using namespace std;
-using ll = long long int;
+
+using ll = long long;
+
 using vi = vector<int>;
-using vvi = vector<vector<int>>; 
+using vvi = vector<vector<int>>;
+using vll = vector<ll>; 
+using vvll = vector<vector<ll>>;
+using vs = vector<string>;
+
+using pii = pair<int, int>;
+using pis = pair<int, string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
 using msi = map<string, int>;
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    ll N;
+    
+    int N;
     cin >> N;
-    vector<ll> A(N);
-    for (ll i = 0;i < N;i++) {
+    vi A(N);
+    for (int i = 0;i < N;i++) {
         cin >> A[i];
         A[i]--;
     }
 
-    vector<ll> ans(N,-1);
-
-    for (int i = 0;i < N;i++) {
-        if (ans[i] != -1) continue;
-        else {
-            ll inans = i;
-            vector<ll> reached;
-            while (inans != A[inans] && ans[inans] == -1) {
-                reached.push_back(inans);
-                inans = A[inans];
-            }
-            reached.push_back(inans);
-
-            for (ll j = 0;j < reached.size();j++) {
-                if (ans[inans] == -1) {
-                    ans[reached[j]] = A[inans] + 1;
-                }
-                else ans[reached[j]] = ans[inans];
-            }
-        }
+    vi ans(N);
+    for (int i = N - 1;i >= 0;i--) {
+        if (i == A[i]) ans[i] = i;
+        else ans[i] = ans[A[i]];
+        ans[i]++;
     }
 
-    for (ll i = 0;i < N;i++) {
+    for (int i = 0;i < N - 1;i++) {
         cout << ans[i] << " ";
     }
-    cout << "\n";
+    cout << ans[N - 1] << "\n";
     return 0;
 }
