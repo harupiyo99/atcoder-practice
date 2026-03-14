0fd35d56b0eaeea3e159856a433a7c3f56da383f
harupiyo99 <harukigcm@gmail.com>
2026-03-14 22:55:14 +0900

    Create: abc449a.cpp

---
+++ ABC/ABC449/abc449_a.cpp
@@ -0,0 +1,37 @@
+#include <iostream>   
+#include <vector>    
+#include <string>    
+#include <algorithm>
+#include <utility> 
+#include <map>       
+#include<queue>
+#include<cmath>
+#include<iomanip>
+
+using namespace std;
+
+using ll = long long;
+
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>; 
+using vvll = vector<vector<ll>>;
+using vs = vector<string>;
+
+using pii = pair<int, int>;
+using pis = pair<int, string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using msi = map<string, int>;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    double D;
+    cin >> D;
+
+    cout << fixed << setprecision(15) << (D / 2) * (D / 2) * 3.141592653589793 << "\n";
+    return 0;
+}
